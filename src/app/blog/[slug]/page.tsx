90163e87684639f31dfb00de06f63354379ee3e1
dasgoutam <goutam2475@gmail.com>
2024-11-12 14:28:28 +0100

    get post from mdx.js

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-//import { getPostBySlug, getAllPostSlugs } from '@/lib/mdx';
+import { getPostBySlug, getAllPostSlugs } from '@/lib/mdx';
 import { MDXRemote } from 'next-mdx-remote/rsc' // Use /rsc for the App Router compatibility
 import { serialize } from 'next-mdx-remote/serialize'
 import fs from 'fs';
@@ -9,18 +9,11 @@ const contentDirectory = path.join(process.cwd(), 'src/content/blogs');
 
 export default async function BlogPost({ params }) {
     const {slug} = await params;
-    const filePath = path.join(contentDirectory, `${slug}.mdx`);
-    const fileContents = fs.readFileSync(filePath, 'utf8');
-  
-    // Parse frontmatter and content using gray-matter
-    const { data, content } = matter(fileContents);
-    console.log("data", content);
-    // Serialize the MDX content to make it usable in the component
-    const mdxSource = await serialize(fileContents, { parseFrontmatter: true });
-    //console.log(mdxSource);
+    const data = await getPostBySlug(slug);
+    
     return (
         <MDXRemote
-            source={content}
+            source={data.content}
             />
     );
 }
--- src/lib/mdx.js
+++ src/lib/mdx.js
@@ -6,16 +6,15 @@ import { serialize } from 'next-mdx-remote/serialize';
 const contentDirectory = path.join(process.cwd(), 'src/content/blogs');
 
 export async function getPostBySlug(slug) {
-  const filePath = path.join(contentDirectory, `${slug}.mdx`);
-  const fileContents = fs.readFileSync(filePath, 'utf8');
+    const filePath = path.join(contentDirectory, `${slug}.mdx`);
+    const fileContents = fs.readFileSync(filePath, 'utf8');
 
-  // Parse frontmatter and content using gray-matter
-  const { data, content } = matter(fileContents);
+    const { data, content } = matter(fileContents);
 
-  // Serialize content for next-mdx-remote
-  const mdxSource = await serialize(content, {parseFrontmatter: true});
-
-  return { props: { mdxSource } };
+    return { 
+        frontmatter: data, 
+        content: content 
+    };
 }
 
 export function getAllPostSlugs() {
